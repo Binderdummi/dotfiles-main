63cb5da7d12be98cae973b865e35c0e87bcae2cb
archibald_baller <archibald@localhost>
2026-04-23 22:41:28 +0200

    Update

--- dot_config/hypr/executable_autostart.sh
+++ dot_config/hypr/executable_autostart.sh
@@ -1,5 +1,5 @@
 hyprpaper &
-waybar &
+waybar -s .config/waybar/mocha/style.css -c .config/waybar/mocha/config.jsonc &
 swaync &
 hyprctl setcursor Bibata-Modern-Ice 24 &
 kanata -c /home/archibald_baller/.kanata.kbd &
--- dot_config/hypr/executable_reload.sh
+++ dot_config/hypr/executable_reload.sh
@@ -5,7 +5,7 @@ pkill kanata
 pkill hypr-dock
 pkill ydotoold
 hyprpaper &
-waybar &
+waybar -s .config/waybar/mocha/style.css -c .config/waybar/mocha/config.jsonc &
 swaync &
 hyprctl setcursor Bibata-Modern-Ice 24 &
 kanata -c /home/archibald_baller/.kanata.kbd &
