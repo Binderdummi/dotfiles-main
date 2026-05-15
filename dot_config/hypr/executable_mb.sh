c6089b975eeb87da936c82c59c64bf3058922ecb
archibald_baller <archibald@localhost>
2026-05-15 19:00:10 +0200

    Sync dotfiles

---
+++ dot_config/hypr/executable_mb.sh
@@ -0,0 +1,2 @@
+hyprctl keyword bind ,mouse:275, exec, ydotool type f
+hyprctl keyword bind ,mouse:276, exec, ydotool type c
---
+++ dot_config/hypr/executable_mbn.sh
@@ -0,0 +1,2 @@
+hyprctl keyword unbind ,mouse:275
+hyprctl keyword unbind ,mouse:276
